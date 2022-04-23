48b45da0d6507668852253602b08477f7065cb5b
Dhiren Vaghani <vaghanidhiren1989@gmail.com>
2022-04-23 17:14:24 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SerilogDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            _logger.LogInformation("LogInformation log");
+            _logger.LogDebug("Log Debug");
+            _logger.LogWarning("Log Warning");
+            _logger.LogError("Log Error");
+            _logger.LogCritical("Log Critical");
+            return View();
+        }
+    }
+}
